9d9b5bc611bb04546566384cab530d5c756a636c
sapnazaware <sapnazaware2023@gmail.com>
2023-12-19 14:55:15 -0600

    portfolio

---
+++ blog/master/master.js
@@ -0,0 +1,14 @@
+
+let init = function () {
+    $('#TopNav').html(TopNav);
+    $('#avatar').html(avatar);
+    $('#post-author').html(postauthor);
+    $('#aem-img').html(AEMimg);
+    $('#MetaButtons').html(MetaButtons);
+    $('#AboutAuthor').html(AboutAuthor);
+    $('#SiteFooter').html(SiteFooter);
+}
+
+$(document).ready(function () {
+    init();
+});
---
+++ blog/master/masterContent.js
@@ -0,0 +1,86 @@
+var TopNav = `<div class="header-section section" id="header-section" name="Header">
+        <div class="widget Header" data-version="2" id="Header1">
+        <div class="header-widget">
+            <a class="header-title header-logo-image" href="/#">
+            AEM BLOG
+            </a>
+        </div>
+        </div>
+        <div class="widget LinkList" data-version="2" id="LinkList99">
+        <div class="menu-widget">
+            <span class="menu-item"><a href="/">Home</a></span><span class="menu-item"><a
+                href="https://webium-blogger.blogspot.com/p/webium-documentations.html">Documentation</a></span>
+        </div>
+        <button aria-label="Menu" class="button-icon menu" type="button"><svg height="24" viewBox="0 0 384.97 384.97"
+            width="24">
+            <path
+                d="M12.03,120.303h360.909c6.641,0,12.03-5.39,12.03-12.03c0-6.641-5.39-12.03-12.03-12.03H12.03 c-6.641,0-12.03,5.39-12.03,12.03C0,114.913,5.39,120.303,12.03,120.303z">
+            </path>
+            <path
+                d="M372.939,180.455H12.03c-6.641,0-12.03,5.39-12.03,12.03s5.39,12.03,12.03,12.03h360.909c6.641,0,12.03-5.39,12.03-12.03 S379.58,180.455,372.939,180.455z">
+            </path>
+            <path
+                d="M372.939,264.667H132.333c-6.641,0-12.03,5.39-12.03,12.03c0,6.641,5.39,12.03,12.03,12.03h240.606 c6.641,0,12.03-5.39,12.03-12.03C384.97,270.056,379.58,264.667,372.939,264.667z">
+            </path>
+            </svg></button>
+        </div>
+ </div>`;
+
+
+ var AEMimg = '<div class="fakeimg"> <img alt="AEM image " src="../master/img/aem_banner.jpeg" /></div>';
+
+var AboutAuthor = `
+        <div class="avatar-container">
+         </div>
+
+          <div class="author-description">
+            <div class="author-name">
+              <span class="description-text">Posted by</span>
+              <div class="authors">Sapna Gawade</div>
+            </div>
+
+            <p class="description-text">Adobe Experience Manager</p>
+        </div>
+`;
+var SiteFooter = `
+<h2></h2>
+`;
+var MetaButtons = `
+        <div  class="meta-button">
+            <a class="twitter"
+              href="https://twitter.com/intent/tweet?url=https://webium-blogger.blogspot.com/2021/03/marketers-get-in-touch-with-your_2.html&amp;text=Marketers: Get in touch with your audience"
+              rel="nofollow noopener noreferer" target="_blank" title="Twitter"><svg height="24" viewBox="0 0 24 24"
+                width="24">
+                <path
+                  d="M22.46,6C21.69,6.35 20.86,6.58 20,6.69C20.88,6.16 21.56,5.32 21.88,4.31C21.05,4.81 20.13,5.16 19.16,5.36C18.37,4.5 17.26,4 16,4C13.65,4 11.73,5.92 11.73,8.29C11.73,8.63 11.77,8.96 11.84,9.27C8.28,9.09 5.11,7.38 3,4.79C2.63,5.42 2.42,6.16 2.42,6.94C2.42,8.43 3.17,9.75 4.33,10.5C3.62,10.5 2.96,10.3 2.38,10C2.38,10 2.38,10 2.38,10.03C2.38,12.11 3.86,13.85 5.82,14.24C5.46,14.34 5.08,14.39 4.69,14.39C4.42,14.39 4.15,14.36 3.89,14.31C4.43,16 6,17.26 7.89,17.29C6.43,18.45 4.58,19.13 2.56,19.13C2.22,19.13 1.88,19.11 1.54,19.07C3.44,20.29 5.7,21 8.12,21C16,21 20.33,14.46 20.33,8.79C20.33,8.6 20.33,8.42 20.32,8.23C21.16,7.63 21.88,6.87 22.46,6Z">
+                </path>
+              </svg></a>
+            <a class="linkedin"
+              href="https://www.linkedin.com/shareArticle?url=https://webium-blogger.blogspot.com/2021/03/marketers-get-in-touch-with-your_2.html"
+              rel="nofollow noopener noreferer" target="_blank" title="LinkedIn"><svg height="24" viewBox="0 0 24 24"
+                width="24">
+                <path
+                  d="M19 3A2 2 0 0 1 21 5V19A2 2 0 0 1 19 21H5A2 2 0 0 1 3 19V5A2 2 0 0 1 5 3H19M18.5 18.5V13.2A3.26 3.26 0 0 0 15.24 9.94C14.39 9.94 13.4 10.46 12.92 11.24V10.13H10.13V18.5H12.92V13.57C12.92 12.8 13.54 12.17 14.31 12.17A1.4 1.4 0 0 1 15.71 13.57V18.5H18.5M6.88 8.56A1.68 1.68 0 0 0 8.56 6.88C8.56 5.95 7.81 5.19 6.88 5.19A1.69 1.69 0 0 0 5.19 6.88C5.19 7.81 5.95 8.56 6.88 8.56M8.27 18.5V10.13H5.5V18.5H8.27Z">
+                </path>
+              </svg></a>
+            <a class="facebook"
+              href="https://www.facebook.com/sharer.php?u=https://webium-blogger.blogspot.com/2021/03/marketers-get-in-touch-with-your_2.html"
+              rel="nofollow noopener noreferer" target="_blank" title="Facebook"><svg height="24" viewBox="0 0 24 24"
+                width="24">
+                <path
+                  d="M12 2.04C6.5 2.04 2 6.53 2 12.06C2 17.06 5.66 21.21 10.44 21.96V14.96H7.9V12.06H10.44V9.85C10.44 7.34 11.93 5.96 14.22 5.96C15.31 5.96 16.45 6.15 16.45 6.15V8.62H15.19C13.95 8.62 13.56 9.39 13.56 10.18V12.06H16.34L15.89 14.96H13.56V21.96A10 10 0 0 0 22 12.06C22 6.53 17.5 2.04 12 2.04Z">
+                </path>
+              </svg></a>
+
+            <button aria-label="Social Share" class="more-menu button-icon" data-a11y-toggle="toggle-social"
+              type="button" id="a11y-toggle-1" aria-controls="toggle-social" aria-expanded="false">
+            </button>
+        </div>
+`;
+var avatar = `
+<img alt="sap" src="../master/img/prof3.jpeg" />
+`;
+
+var postauthor = `
+<strong>Sapna Gawade</strong>
+`;
